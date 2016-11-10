e582e66bd9fb6b6f2827d51219bc6c4d0e86b97e
Grimault Yannick <ogamix2013@gmail.com>
2016-11-11 00:44:32 +0100

    Cleaned up the code a bit

--- Assignment4_Centralized_Agents/src/CentralizedTemplate.java
+++ Assignment4_Centralized_Agents/src/CentralizedTemplate.java
@@ -38,7 +38,8 @@ public class CentralizedTemplate implements CentralizedBehavior {
     private long timeout_setup;
     private long timeout_plan;
     private Random random;
-    private static final int MAX_ITER = 20000;
+    private static final int MAX_ITER = 100000;
+    private static final int MAX_NEIGHS = 100;
     private static final double PROBA_PERMUTE = 0.85;
 
     /*
@@ -113,29 +114,32 @@ public class CentralizedTemplate implements CentralizedBehavior {
         List<List<CustomAction>> optimalPlan = copyPlan(nodePlans);
         List<List<CustomAction>> tmpPlan = copyPlan(nodePlans);
         List<List<CustomAction>> neighPlan = copyPlan(nodePlans);
+
         int nbIterWithNoChange = 0;
         // Main loop
         for (int i = 0; i < MAX_ITER; i++) {
-//            if (nbIterWithNoChange < 1000) {
-//                nodePlans = copyPlan(optimalPlan);
-//            }
+            if (nbIterWithNoChange < 100) {
+                nodePlans = copyPlan(optimalPlan);
+            }
 
             // Local exploration to find the "best" neighbour
-            for (int j = 0; j < 50; j++) {
+            for (int j = 0; j < MAX_NEIGHS; j++) {
                 neighPlan = copyPlan(nodePlans);
                 update2(neighPlan, vehicles, tasks);
                 if(totalCost(neighPlan, vehicles) < totalCost(tmpPlan, vehicles)){
                     tmpPlan = copyPlan(neighPlan);
                 }
             }
-            nodePlans = copyPlan(tmpPlan);
-            //update(nodePlans, vehicles, tasks);
 
+            // Move to the best one, even if it doesn't improve
+            nodePlans = copyPlan(tmpPlan);
+
+            // We stay at the optimal plan to explore its neighborhood and minimize it
             if (totalCost(nodePlans, vehicles) < totalCost(optimalPlan, vehicles)) {
                 optimalPlan = copyPlan(nodePlans);
-//                nbIterWithNoChange = 0;
-//            } else {
-//                nbIterWithNoChange += 1;
+                nbIterWithNoChange = 0;
+            } else {
+                nbIterWithNoChange += 1;
             }
 
             // If we're too close to the end, we need to stop.
@@ -330,21 +334,30 @@ public class CentralizedTemplate implements CentralizedBehavior {
         }
     }
 
+    /*
+    Alternate implementation of the update function.
+    Now, it moves a random task to a random vehicle at a random position (always with respect to the constraints)
+     */
     private void update2(List<List<CustomAction>> plan, List<Vehicle> vehicles, TaskSet tasks){
         List<Task> tasksList = new ArrayList<>(tasks);
+
+        // Get a random task, and compute both its pickup action and its delivery action
         Task chosenTask = tasksList.get(random.nextInt(tasks.size()));
         CustomAction pickupAction = new CustomAction(true, chosenTask);
         CustomAction deliveryAction = new CustomAction(false, chosenTask);
-        Vehicle currentVehicle = vehicle(plan,pickupAction,vehicles);
 
+        // Remove both actions from current vehicle
+        Vehicle currentVehicle = vehicle(plan,pickupAction,vehicles);
         int vehicleId = vehicles.indexOf(currentVehicle);
         plan.get(vehicleId).remove(pickupAction);
         plan.get(vehicleId).remove(deliveryAction);
 
+        // Get a new vehicle for insertion
         int newVehicleId = random.nextInt(vehicles.size());
         Vehicle newVehicle = vehicles.get(newVehicleId);
         List<CustomAction> vehiclePlan = plan.get(newVehicleId);
 
+        // We'll need to explore the plan of the vehicle from start to the chosen position to check the capacity
         int cumulWeight = 0;
         int id = 0;
 
@@ -352,11 +365,13 @@ public class CentralizedTemplate implements CentralizedBehavior {
         boolean pickupTaskPlaced = false;
 
         while(!pickupTaskPlaced){
+            // If it happens that we reach the end of the list, we place the task here
             if(id == vehiclePlan.size()){
                 vehiclePlan.add(id, pickupAction);
                 break;
             }
 
+            // We need to update the carried weight up until this action
             if(id < newPickupId) {
                 CustomAction currentAction = vehiclePlan.get(id);
                 if (currentAction.isPickUp) {
@@ -367,6 +382,8 @@ public class CentralizedTemplate implements CentralizedBehavior {
                 id += 1;
             }
 
+            // When reaching the chosen position, we check if the vehicle can pickup the task at this point.
+            // Otherwise we increment the chosen position to pick it up later
             if(id == newPickupId){
                 if (cumulWeight + pickupAction.task.weight < newVehicle.capacity()){
                     vehiclePlan.add(id, pickupAction);
@@ -377,15 +394,20 @@ public class CentralizedTemplate implements CentralizedBehavior {
             }
         }
 
+        id = newPickupId;
+
+        // Now that we placed the pick up action, we need to place the delivery action after that, without carrying too much weight in the meantime
         int newDeliveryId = newPickupId + random.nextInt(vehiclePlan.size() - newPickupId) + 1;
         boolean deliveryTaskPlaced = false;
-        id = newPickupId;
+
         while(!deliveryTaskPlaced){
+            // If it happens that we reach the end of the list, we place the task here
             if(id == vehiclePlan.size()){
                 vehiclePlan.add(id, pickupAction);
                 break;
             }
 
+            // We need to check if we're not going to carry too much weight, in which case we need to deliver the new task before continuing
             if(id < newDeliveryId){
                 CustomAction currentAction = vehiclePlan.get(id);
                 if (currentAction.isPickUp) {
@@ -402,6 +424,7 @@ public class CentralizedTemplate implements CentralizedBehavior {
                 id += 1;
             }
 
+            // When we reach the chosen position, there is no drawback to place the delivery action here.
             if(id == newDeliveryId){
                 vehiclePlan.add(deliveryAction);
                 deliveryTaskPlaced = true;
@@ -410,7 +433,7 @@ public class CentralizedTemplate implements CentralizedBehavior {
     }
 
     /*
-    Computes the initial list of action where the first vehicle takes one task at a time and delivers them all
+    Computes the initial plan where the vehicles take the tasks at random and carry them one at a time.
      */
     private List<List<CustomAction>> naivePlan(TaskSet tasks, List<Vehicle> vehicles) {
         List<List<CustomAction>> plan = new ArrayList<>();
